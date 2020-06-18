b78c8ede103d5174ecd99e99529eae0daf62d44d
Ian Smith <iansmith@acm.org>
2020-06-18 11:28:21 -0400

    .

--- build-tools.bash
+++ build-tools.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
-set -e
+set -ex
 
 CMAKE_VERSION="3.17.3"
 CMAKE_URL="https://codeload.github.com/Kitware/CMake/tar.gz/v${CMAKE_VERSION}"
 EXPAT_URL="git@github.com:libexpat/libexpat.git"
 JSONCPP_URL="git@github.com:open-source-parsers/jsoncpp.git"
 NINJA_URL="git://github.com/ninja-build/ninja.git"
+ZLIB_VERSION="1.2.11"
+ZLIB_URL="https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz"
+BZIP2_VERSION="1.0.8"
+BZIP2_URL="https://sourceware.org/pub/bzip2/bzip2-${BZIP2_VERSION}.tar.gz"
+
+XZ_VERSION="5.2.5"
+XZ_URL="https://downloads.sourceforge.net/project/lzmautils/xz-5.2.5.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Flzmautils%2Ffiles%2Fxz-5.2.5.tar.gz%2Fdownload&ts=1592486495"
 
 OS=""
 TOOLSDIR=""
@@ -92,6 +99,48 @@ function darwin_jsoncpp_install() {
   return 0
 }
 
+function darwin_zlib_install() {
+  echo =================== installing zlib from ${ZLIB_URL}
+  mkdir -p src
+  file="zlib-${ZLIB_VERSION}src.tar.gz"
+	curl -o ./src/${file} "${ZLIB_URL}"
+	cd src
+	tar xzf ${file}
+	mkdir -p ../build/darwin-zlib
+	cd ../build/darwin-zlib
+	PATH=${TOOLSDIR}/bin:$PATH ../../src/zlib-${ZLIB_VERSION}/configure --static --prefix="$TOOLSDIR"
+	make install
+	cd ../..
+}
+
+function darwin_xz_install() {
+  echo =================== installing xz from ${XZ_URL}
+  mkdir -p src
+  file="xz-${XZ_VERSION}src.tar.gz"
+	curl -o ./src/${file} "${XZ_URL}"
+	cd src
+	tar xzf ${file}
+	mkdir -p ../build/darwin-xz
+	cd ../build/darwin-xz
+	PATH=${TOOLSDIR}/bin:$PATH ../../src/zlib-${XZ_VERSION}/configure --static --prefix="$TOOLSDIR"
+	make install
+	cd ../..
+}
+
+#must build in tree, it's shell scripts
+function darwin_bzip2_install() {
+  echo =================== installing bzip2 from ${BZIP2_URL}
+  mkdir -p src
+  file="bzip2-${BZIP2_VERSION}src.tar.gz"
+	curl -o ./src/${file} "${BZIP2_URL}"
+	cd src
+	tar xzf ${file}
+	cd bzip2-${BZIP2_VERSION}
+	PATH=${TOOLSDIR}/bin:$PATH make
+	PATH=${TOOLSDIR}/bin:$PATH make install PREFIX="$TOOLSDIR"
+	cd ../..
+}
+
 
 function darwin_cmake_install() {
   echo =================== installing cmake from ${CMAKE_URL}
@@ -118,11 +167,14 @@ if [ "$OS" == "Darwin" ]; then
     echo unable to determine where the tools dir is, aborting
     exit 1
   fi
-  darwin_meson_install
-  darwin_ninja_install
-  darwin_expat_install
-  darwin_jsoncpp_install
-  darwin_expat_install
+  #darwin_meson_install
+  #darwin_ninja_install
+  #darwin_expat_install
+  #darwin_jsoncpp_install
+  #darwin_zlib_install
+  #darwin_bzip2_install
+  darwin_xz_install
+  #darwin_cmake_install
 else
 	echo feelings from scratch only works on Darwin right now
 fi
